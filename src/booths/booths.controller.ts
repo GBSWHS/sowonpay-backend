3eb914c62d35e9c84b1c8123f4d78b2834f7e134
Minhyeok Park <opensource@pmh.codes>
2022-09-29 17:22:56 +0900

    fix: set timer to starts with 0

--- src/booths/booths.controller.ts
+++ src/booths/booths.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, MessageEvent, Param, Sse, UseGuards } from '@nestjs/common'
-import { concatMap, interval, Observable } from 'rxjs'
+import { concatMap, Observable, timer } from 'rxjs'
 import { PointSseEvent, PointSseService } from '../transactions/sse/PointSse.service'
 import { UserGuard } from '../users/users.guard'
 import { BoothService } from './booths.service'
@@ -19,7 +19,7 @@ export class BoothController {
 
   @Sse('@rank')
   public getRank (): Observable<MessageEvent> {
-    return interval(10 * 1000)
+    return timer(0, 10 * 1000)
       .pipe(concatMap(async () => ({
         data: await this.boothService.getRank()
       })))
