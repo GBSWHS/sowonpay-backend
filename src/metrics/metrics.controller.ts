627cb6eb38aab84e7fcebc77c7b1636f9662cfba
Minhyeok Park <opensource@pmh.codes>
2022-10-25 16:54:50 +0900

    feat: add metric guard

--- src/metrics/metrics.controller.ts
+++ src/metrics/metrics.controller.ts
@@ -1,18 +1,33 @@
-import { Controller, Res, Sse, UseGuards } from '@nestjs/common'
+import { Controller, ForbiddenException, Get, Res, Sse, UseGuards } from '@nestjs/common'
 import { Response } from 'express'
 import { UserGuard } from '../users/users.guard'
+import { Metrics, MetricsService } from './metrics.service'
 import { MetricSseEvent, MetricSseService } from './sse/MetricSse.service'
 
 @Controller('metrics')
 export class MetricsController {
   constructor (
-    private readonly metricSseService: MetricSseService
+    private readonly metricSseService: MetricSseService,
+    private readonly metricService: MetricsService
   ) {}
 
   @Sse('@sse')
   @UseGuards(UserGuard)
   public createMetricSSE (@Res({ passthrough: true }) res: Response): MetricSseEvent {
-    console.log(res.locals.user)
+    if (!(res.locals.user.isAdmin as boolean)) {
+      throw new ForbiddenException('NOT_AN_ADMIN')
+    }
+
     return this.metricSseService.subscribe()
+  }
+
+  @Get()
+  @UseGuards(UserGuard)
+  public async getMetricInfo (@Res({ passthrough: true }) res: Response): Promise<Metrics> {
+    if (!(res.locals.user.isAdmin as boolean)) {
+      throw new ForbiddenException('NOT_AN_ADMIN')
+    }
+
+    return await this.metricService.getMetrics()
   }
 }
--- src/metrics/metrics.service.ts
+++ src/metrics/metrics.service.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Booths } from 'src/booths/entity/Booths'
+import { Transactions } from 'src/transactions/entity/Transactions'
+import { Users } from 'src/users/entity/Users'
+import { IsNull, Repository } from 'typeorm'
+
+export interface Metrics {
+  holds: number
+  imports: number
+  exports: number
+}
 
 @Injectable()
-export class MetricsService {}
+export class MetricsService {
+  constructor (
+    @InjectRepository(Users)
+    private readonly users: Repository<Users>,
+    @InjectRepository(Booths)
+    private readonly booths: Repository<Booths>,
+    @InjectRepository(Transactions)
+    private readonly trasactions: Repository<Transactions>
+  ) {}
+
+  public async getMetrics (): Promise<Metrics> {
+    const userHolds = await this.users.find({
+      select: {
+        point: true
+      }
+    })
+
+    const bankGives = await this.trasactions.find({
+      select: {
+        point: true
+      },
+      where: {
+        boothId: IsNull()
+      }
+    })
+
+    const boothHolds = await this.booths.find({
+      select: {
+        point: true
+      }
+    })
+
+    return {
+      holds: userHolds.reduce((prev, curr) => prev + curr.point, 0),
+      imports: bankGives.reduce((prev, curr) => prev + curr.point, 0),
+      exports: boothHolds.reduce((prev, curr) => prev + curr.point, 0)
+    }
+  }
+}
