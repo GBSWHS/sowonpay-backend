9b98a40bffd459ea8b451edec8218e0d8c1aff59
Minhyeok Park <opensource@pmh.codes>
2022-10-25 17:00:09 +0900

    fix: import entity

--- src/metrics/metrics.module.ts
+++ src/metrics/metrics.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Booths } from '../booths/entity/Booths'
+import { Transactions } from '../transactions/entity/Transactions'
+import { Users } from '../users/entity/Users'
 import { MetricsController } from './metrics.controller'
 import { MetricsService } from './metrics.service'
 import { MetricSseService } from './sse/MetricSse.service'
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Users, Booths, Transactions])
+  ],
   controllers: [MetricsController],
   providers: [
     MetricsService,
